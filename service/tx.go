bde863a92e33491b328e343f826c8be32720fef0
wureny <wurenyu19@gmail.com>
2024-01-19 15:43:18 +0800

    fix: cancel the operation of getting op in the SaveTransaction interface

--- service/tx.go
+++ service/tx.go
@@ -10,30 +10,12 @@ func SaveTransaction(info *models.SavedTr) error {
 	if info.UserOperationHash == "" {
 		return global.OtherError("UserOperationHash is empty")
 	}
-
 	var chain models.Chain
 	err := dao.GetChainByNetworkId(&chain, info.NetworkId)
-
-	resInDb, err := dao.FindTransaction(&models.Transaction{
-		ChainId:           chain.ID,
-		UserOperationHash: info.UserOperationHash,
-	})
 	if err != nil {
 		return err
 	}
-	if len(resInDb) > 0 {
-		return global.OtherError("UserOperationHash already exists")
-	}
-
-	response, err := GetUserOperationByHash(chain.BundlerApi, info.UserOperationHash)
-
 	tx := &models.Transaction{
-		BlockHash:         response.Result.BlockHash,
-		BlockNumber:       response.Result.BlockNumber,
-		TxHash:            response.Result.TransactionHash,
-		Sender:            response.Result.UserOperation.Sender,
-		EntryPointAddress: response.Result.Entrypoint,
-		UserOperation:     response.Result.UserOperation,
 		UserOperationHash: info.UserOperationHash,
 		Type:              info.Type,
 		Status:            models.TransactionStatusInit,
